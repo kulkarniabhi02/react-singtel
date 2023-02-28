c6a2337d45ce88e896ddf93df80bac0641bd65ea
kulkarniabhi0211 <104963617+kulkarniabhi0211@users.noreply.github.com>
2023-02-28 18:22:54 +0530

    Updated home page name

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, BrowserRouter, Routes, Route } from "react-router-dom";
 import Launch from "./components/launch/launch.component";
 import LeaderBoardComponent from "./components/leaderboard/leaderboard.component";
 import Word from "./components/word/word.component";
@@ -7,13 +7,13 @@ import Word from "./components/word/word.component";
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Launch />} />
         <Route path="/word" element={<Word />} />
         <Route path="/leaderboard" element={<LeaderBoardComponent />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
